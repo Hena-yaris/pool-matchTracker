4bed24a2ba3ff64bc9a5bdce6a458552a63fd1ce
Hena <meraey21@gmail.com>
2025-10-05 13:24:43 +0300

    updated match setup

--- client/src/pages/MatchSetup.jsx
+++ client/src/pages/MatchSetup.jsx
@@ -6,19 +6,36 @@ const MatchSetup = () => {
   const [players, setPlayers] = useState([{ name: "" }]);
 
   const handleChange = (index, value) => {
-    const updated = [...players];
-    updated[index].name = value;
-    setPlayers(updated);
+        const updated = [...players];
+        updated[index].name = value;
+        setPlayers(updated);
+   
   };
 
   const addPlayer = () => {
-    setPlayers([...players, { name: "" }]);
+    // Get the last player in the array
+    const lastPlayer = players[players.length - 1];
+
+    // Only add a new input if the last one is not empty
+    if (lastPlayer.name.trim() !== "") {
+      setPlayers([...players, { name: "" }]);
+    }
   };
 
+  const removePlayer = (index)=> {
+    const updated = [...players];
+    updated.splice(index,1);
+    setPlayers(updated);
+  }
+
   const startGame = () => {
-    // Send players to game screen
-    navigate("/game", { state: { players } });
+    const hasEmpty = players.some((p) => p.name.trim() === "");
+    if (!hasEmpty) {
+      navigate("/game", { state: { players } });
+    } 
+
   };
+
 
   return (
     <>
@@ -27,32 +44,47 @@ const MatchSetup = () => {
 
         <div className="flex flex-col gap-4 w-full max-w-md">
           {players.map((player, index) => (
-            <input
-              key={index}
-              type="text"
-              placeholder={`Player ${index + 1} Name`}
-              value={player.name}
-              onChange={(e) => handleChange(index, e.target.value)}
-              className="px-4 py-2 rounded-xl text-black"
-            />
+            <div key={index} className="flex gap-2">
+              <input
+                type="text"
+                placeholder={`Player ${index + 1} Name`}
+                value={player.name}
+                onChange={(e) => handleChange(index, e.target.value)}
+                className={`px-4 py-2 rounded-xl border border-gray-400 w-full focus:outline-none focus:ring-2 focus:ring-yellow-500 text-black bg-gray-200`}
+              />
+              {players.length>1 && (
+                <button 
+                onClick={()=> removePlayer(index)}
+                className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-full">
+                    X
+                </button>
+              )}
+            </div>
           ))}
 
           <button
             onClick={addPlayer}
-            className="bg-yellow-500 hover:bg-yellow-600 px-4 py-2 rounded-xl font-semibold"
+            className={` px-4 py-2 rounded-xl font-semibold ${
+              players[players.length - 1].name.trim() === ""
+                ? "bg-yellow-300 cursor-not-allowed"
+                : "bg-yellow-500 hover:bg-yellow-600"
+            }`}
           >
             + Add Player
           </button>
 
           <button
             onClick={startGame}
-            className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded-xl font-semibold"
+            className={`px-4 py-2 rounded-xl font-semibold ${
+              players[players.length - 1].name.trim() === ""
+                ? "bg-green-300 cursor-not-allowed"
+                : "bg-green-500 hover:bg-green-600"
+            }`}
           >
             Start Game
           </button>
         </div>
       </div>
-
     </>
   );
 };
